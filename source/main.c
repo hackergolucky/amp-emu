8efc3aac255502a02815361b181d1556c1a5327f
Avery Ramsey <hackergolucky@gmail.com>
2021-11-06 14:07:49 -0400

    Initial commit, draws a window

---
+++ source/main.c
@@ -0,0 +1,54 @@
+/*  Amp - A GBA emulator written in C/C++
+ *  Copyright (C) 2021 hackergolucky (Avery Ramsey)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+
+int main(int argc, char *argv[])
+{
+    int retval = 0;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("Amp GBA Emulator",
+                                            SDL_WINDOWPOS_CENTERED,
+                                            SDL_WINDOWPOS_CENTERED,
+                                            1280, 720,
+                                            SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    if(!window)
+    {
+        retval = 1;
+        goto end;
+    }
+
+    while(true)
+    {
+        SDL_Event e;
+        while(SDL_PollEvent(&e))
+        {
+            if(e.type == SDL_QUIT)
+                goto destroy;
+        }
+    }
+
+    destroy:
+        SDL_DestroyWindow(window);
+    end:
+        SDL_Quit();
+        return retval;
+}
